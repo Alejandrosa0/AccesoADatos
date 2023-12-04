e6d73cf9de50aa3de103b7f2f7b06fd00cc4899c
Alejandrosa0 <132493184+Alejandrosa0@users.noreply.github.com>
2023-12-05 00:52:54 +0100

    Add files via upload

---
+++ PRACTICA1_4/src/practica1_4/UsaAccesoDOM.java
@@ -0,0 +1,22 @@
+package practica1_4;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.ParserConfigurationException;
+import org.xml.sax.SAXException;
+
+public class UsaAccesoDOM {
+
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+        AccesoDOM a = new AccesoDOM(); //Creación de instancia de la clase AccesoDOM y objeto File con el nombre del archivo xml ("Libros.xml")
+        File f = new File("Libros.xml"); //Necesitamos Libros.xml en la ruta correcta
+
+        a.abriXMLaDOM(f); //Llamamos a los métodos para abrir y recorrer el archivo XML y mostramos el contenido del mismo
+        a.recorreDOMyMuestra();
+        
+        a.insertarLibroEnDOM("Yerma", "Lorca", "1935"); //Llamamos a los métodos para insertar un libro en el archivo XML y eliminar un libro (nodo) del archivo XML
+        a.deleteNode("Don Quijote"); 
+        
+        a.guardarDOMcomoArchivo("LibrosDeDOM.xml"); //Guardará el documento modificado en un archivo llamado ("LibrosDeDOM.xml")
+    }
+}
