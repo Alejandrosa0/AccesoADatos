69faa641b9d3f330bd1c5c0d93093cd76c7b434c
Alejandrosa0 <132493184+Alejandrosa0@users.noreply.github.com>
2023-12-05 00:44:16 +0100

    Add files via upload

---
+++ PROYECTO_CONEXIONBDEx/src/main/java/com/mycompany/proyecto_conexionbdex/PROYECTO_CONEXIONBDEx.java
@@ -0,0 +1,8 @@
+package com.mycompany.proyecto_conexionbdex;
+
+public class PROYECTO_CONEXIONBDEx {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
