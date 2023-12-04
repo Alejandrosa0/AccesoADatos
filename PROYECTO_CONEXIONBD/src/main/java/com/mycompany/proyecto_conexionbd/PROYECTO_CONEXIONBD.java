d225a928e0d8432c38e0ab7445b1ac4d7eabe856
Alejandrosa0 <132493184+Alejandrosa0@users.noreply.github.com>
2023-12-05 00:56:09 +0100

    Add files via upload

---
+++ PROYECTO_CONEXIONBD/src/main/java/com/mycompany/proyecto_conexionbd/PROYECTO_CONEXIONBD.java
@@ -0,0 +1,8 @@
+package com.mycompany.proyecto_conexionbd;
+
+public class PROYECTO_CONEXIONBD {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
